e2f9f16ab83f1a07f3244e99006eaac5f13aa3fa
Abdul_Muheeth <abdul.muheeth@smartdocs.ai>
2023-05-04 15:57:32 +0530

    connected to database

--- src/main/java/com/xapp/xjava/XJavaApplication.java
+++ src/main/java/com/xapp/xjava/XJavaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class XJavaApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(XJavaApplication.class, args);
 	}
