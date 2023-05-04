b2973351a8359137490838e506a845d23126712c
Abdul_Muheeth <abdul.muheeth@smartdocs.ai>
2023-05-04 12:51:47 +0530

    initial commit

---
+++ src/main/java/com/xapp/xjava/XJavaApplication.java
@@ -0,0 +1,13 @@
+package com.xapp.xjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xapp/xjava/XJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xapp.xjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
